b8eeefde132370069dcbd4c1e0c80111aacb83f4
ReeseHatfield <hatfield.69@wright.edu>
2025-09-28 11:38:33 -0400

    sep 26

--- Week-05-Lists/Reese/ListTypes/src/Driver.java
+++ Week-05-Lists/Reese/ListTypes/src/Driver.java
@@ -2,24 +2,41 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+
 public class Driver {
     public static void main(String[] args) {
-        List<String> l1 = new ArrayList<>();
+        // List<String> l1 = new ArrayList<>();
 
-        List<String> l2 = new LinkedList<>();
-
-
-
-        Node<String> n1 = new Node<String>("CEG 2350");
-        Node<String> n2 = new Node<String>("CS 1150");
-        Node<String> n3 = new Node<String>("CS 1181");
+        // List<String> l2 = new LinkedList<>();
         
-        n1.setNext(n2);
-        n2.setNext(n3);
-        n3.setNext(null);
+
+        CustomLinkedList<String> customList = new CustomLinkedList<>();
+
+        customList.add("Hello");
+        customList.add("World");
+        customList.add("Thing1");
+        customList.add("Thing2");
+        customList.add("Thing3");
+        customList.add("Thing4");
+        customList.add("Thing5");
+        customList.add("Thing6");
+
+        System.out.println(customList.get(4));
 
 
-        System.out.println(n1.getNext().getNext());
+
+        
+
+        // Node<String> n1 = new Node<String>("CEG 2350");
+        // Node<String> n2 = new Node<String>("CS 1150");
+        // Node<String> n3 = new Node<String>("CS 1181");
+        
+        // n1.setNext(n2);
+        // n2.setNext(n3);
+        // n3.setNext(null);
+
+
+        // System.out.println(n1.getNext().getNext());
 
     }
 }
