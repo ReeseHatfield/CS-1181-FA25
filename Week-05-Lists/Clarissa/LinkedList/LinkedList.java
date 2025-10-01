2a526867bbdb255d10d6b3ca23a6ace0f35aba28
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2025-10-01 12:11:29 -0400

    linked list, stacks, queues

--- Week-05-Lists/Clarissa/LinkedList/LinkedList.java
+++ Week-05-Lists/Clarissa/LinkedList/LinkedList.java
@@ -1,12 +1,15 @@
 public class LinkedList<E> {
     private Node<E> head;
+    private int size = 0;
 
     public LinkedList() {
         head = null;
+        size = 0;
     }
 
     public LinkedList(E headValue) {
         head = new Node<>(headValue);
+        size = 1;
     }
 
     public void add(E value) {
@@ -21,5 +24,54 @@ public class LinkedList<E> {
             }
             currNode.setNext(newNode);
         }
+        size++;
+    }
+
+    public E get(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            int currIndex = 0;
+            Node<E> currNode = head;
+            while (currIndex < index) {
+                currIndex++;
+                currNode = currNode.getNext();
+            }
+            return currNode.getValue();
+        }
+    }
+
+    public void replace(int index, E newValue) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            int currIndex = 0;
+            Node<E> currNode = head;
+            while (currIndex < index) {
+                currIndex++;
+                currNode = currNode.getNext();
+            }
+            currNode.setValue(newValue);
+        }
+    }
+
+    @Override
+    public String toString() {
+        String list = "[";
+
+        if (head == null) {
+            list = "[]";
+        } else {
+            Node<E> currNode = head;
+            for (int i = 0; i < size; i++) {
+                if (i == (size - 1)) {
+                    list = list + currNode.getValue() + "]";
+                } else {
+                    list = list + currNode.getValue() + ", ";
+                }
+                currNode = currNode.getNext();
+            }
+        }
+        return list;
     }
 }
--- Week-05-Lists/Clarissa/LinkedList/LinkedListDriver.java
+++ Week-05-Lists/Clarissa/LinkedList/LinkedListDriver.java
@@ -1,9 +1,42 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
 public class LinkedListDriver
 {
     public static void main(String[] args)
     {
         LinkedList<String> labTAs = new LinkedList<>();
-        labTAs.add("Greg");
         System.out.println(labTAs);
+        labTAs.add("Greg");
+        labTAs.add("Subiksha");
+        labTAs.add("Blake");
+        labTAs.add("Matthew");
+        System.out.println(labTAs);
+        System.out.println(labTAs.get(3));
+        labTAs.replace(3, "Shepard");
+        System.out.println(labTAs);
+
+        Stack<String> discardPile = new Stack<>();
+
+        discardPile.push("Ace of Diamonds");
+        discardPile.push("10 of Spades");
+        discardPile.push("9 of Hearts");
+        System.out.println(discardPile);
+        System.out.println(discardPile.pop());
+        System.out.println(discardPile);
+        System.out.println(discardPile.peek());
+        System.out.println(discardPile);
+
+        Queue<String> customerOrder = new ArrayDeque<>();
+        customerOrder.offer("Bob");
+        customerOrder.offer("Karen");
+        customerOrder.offer("Tom");
+        customerOrder.offer("Kayleigh");
+        System.out.println(customerOrder);
+        System.out.println(customerOrder.poll());
+        System.out.println(customerOrder);
+        System.out.println(customerOrder.peek());
+        System.out.println(customerOrder);
     }
 }
