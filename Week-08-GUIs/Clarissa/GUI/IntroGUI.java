5022bcadd33d7bba3c11edaf81ea47e90128e94b
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2025-10-13 16:57:08 -0400

    gui intro

---
+++ Week-08-GUIs/Clarissa/GUI/IntroGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class IntroGUI
+{
+    public static void main(String[] args)
+    {
+        JFrame mainWindow = new JFrame("Welcome to GUI!");
+        mainWindow.setSize(700, 500);
+        mainWindow.setLocation(100, 100);
+        mainWindow.setVisible(true);
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
