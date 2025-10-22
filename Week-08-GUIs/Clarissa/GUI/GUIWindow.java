76c00be377e47f8a3cc9aea3e9c9f02e6d680b07
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2025-10-22 11:10:13 -0400

    more gui

--- Week-08-GUIs/Clarissa/GUI/GUIWindow.java
+++ Week-08-GUIs/Clarissa/GUI/GUIWindow.java
@@ -7,18 +7,18 @@ public class GUIWindow extends JFrame
 {
     private int clickCount = 0;
     private int multiplier = 1;
+    private JPanel mainPanel = new JPanel(new BorderLayout());
+    private JSlider fontSizeSlider = new JSlider();
+    private JLabel instructions = new JLabel("Click the button below to get points!");
+    private JLabel score = new JLabel("Score: ");
 
     public GUIWindow(String title)
     {
         super(title);
 
-        JPanel mainPanel = new JPanel(new BorderLayout());
         JPanel manyButtonPanel = new JPanel(new GridLayout(3, 10));
         JPanel centralPanel = new JPanel();
         centralPanel.setLayout(new BoxLayout(centralPanel, BoxLayout.Y_AXIS));
-
-        JLabel instructions = new JLabel("Click the button below to get points!");
-        JLabel score = new JLabel("Score: ");
 
         JButton cookieClicker = new JButton("Click me!");
         cookieClicker.addActionListener(new ActionListener()
@@ -35,11 +35,31 @@ public class GUIWindow extends JFrame
         JLabel multiplierLabel = new JLabel();
         multiplierLabel.setText("Multiplier: 1");
 
+        JLabel multiplierTimer = new JLabel("Timer");
+
         JButton multiplierButton = new JButton();
         multiplierButton.setText("Multiplier");
         multiplierButton.addActionListener(e -> {
+            this.setTitle("MULTIPLYING!!!");
             multiplier++;
             multiplierLabel.setText("Multiplier: " + multiplier);
+
+            for (int i = 5; i > 0; i--)
+            {
+                multiplierTimer.setText("Time remaining: " + i + "s");
+                centralPanel.revalidate();
+                centralPanel.repaint();
+
+                try
+                {
+                    Thread.sleep(1000);
+                }
+                catch (InterruptedException ie)
+                {
+                    System.out.println("Oops!");
+                }
+            }
+            multiplierTimer.setText("");
         });
 
         // sometimes directly setting the size of a component via setSize
@@ -49,6 +69,7 @@ public class GUIWindow extends JFrame
         centralPanel.add(instructions);
         centralPanel.add(score);
         centralPanel.add(multiplierLabel);
+        centralPanel.add(multiplierTimer);
         centralPanel.add(multiplierButton);
         centralPanel.add(cookieClicker);
         mainPanel.add(centralPanel, BorderLayout.CENTER);
@@ -75,7 +96,25 @@ public class GUIWindow extends JFrame
             manyButtonPanel.add(button);
         }
 
+        sliderSetup();
+        mainPanel.add(fontSizeSlider, BorderLayout.EAST);
         mainPanel.add(manyButtonPanel, BorderLayout.SOUTH);
         this.add(mainPanel);
+    }
+
+    private void sliderSetup()
+    {
+        fontSizeSlider.setMajorTickSpacing(3);
+        fontSizeSlider.setMinorTickSpacing(1);
+        fontSizeSlider.setMaximum(30);
+        fontSizeSlider.setMinimum(1);
+        fontSizeSlider.setName("font size selector");
+        fontSizeSlider.setPaintTicks(true);
+        fontSizeSlider.setPaintLabels(true);
+        fontSizeSlider.setValue(5);
+        fontSizeSlider.addChangeListener(e -> {
+            int size = fontSizeSlider.getValue();
+            instructions.setFont(new Font("Comic Sans MS", Font.ITALIC, size));
+        });
     }
 }
