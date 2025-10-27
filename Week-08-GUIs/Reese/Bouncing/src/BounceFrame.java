394351ea798e019f902641ce5ef7480dada187d3
ReeseHatfield <reesehatfield02@gmail.com>
2025-10-27 11:36:33 -0400

    'oct 27

--- Week-08-GUIs/Reese/Bouncing/src/BounceFrame.java
+++ Week-08-GUIs/Reese/Bouncing/src/BounceFrame.java
@@ -1,6 +1,7 @@
-import javax.naming.NameAlreadyBoundException;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class BounceFrame extends JFrame {
     
@@ -16,13 +17,55 @@ public class BounceFrame extends JFrame {
 
         this.root.add(b);
 
+        moveWithArrowKeys();
 
-        animate();
+        // animate();
+
+
 
         this.setTitle("Bouncing Ball");
         this.setSize(500, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
+    }
+
+
+    private void moveWithArrowKeys(){
+
+
+        this.b.setFocusable(true); 
+        this.b.requestFocusInWindow();
+
+        KeyListener keyInput = new KeyListener() {
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // key down
+                if(e.getKeyCode() == KeyEvent.VK_LEFT){
+                    b.x -= 50;
+                }
+                else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+                    b.x += 50;
+                }
+
+                b.repaint();
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // key up
+
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                //
+            }
+            
+        };
+
+
+        this.b.addKeyListener(keyInput);
     }
 
     private int deltaX = 5;
@@ -49,9 +92,9 @@ public class BounceFrame extends JFrame {
             if(b.x < minX || b.x + b.diameter > maxX){
                 System.out.println("gone off in the x");
                 deltaX *= -1;
-                JFrame f = new JFrame();
-                f.setSize(20,20);
-                f.setVisible(true);
+                // JFrame f = new JFrame();
+                // f.setSize(20,20);
+                // f.etVisible(true);
             }
 
 
@@ -66,4 +109,9 @@ public class BounceFrame extends JFrame {
         t.start();
 
     }
+
+    // b.setFocusable(true); 
+    // b.requestFocusInWindow();
+
+
 }
