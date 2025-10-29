9b94e551246c1fd9ef4fe7f01d7cf3bb61cd9c5e
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2025-10-29 12:11:31 -0400

    recursion

--- Week-10-Recursion/Clarissa/RecursionPractice/RecursionPractice.java
+++ Week-10-Recursion/Clarissa/RecursionPractice/RecursionPractice.java
@@ -3,6 +3,7 @@ public class RecursionPractice
     public static void main(String[] args)
     {
         System.out.println(reverseWord("hello"));
+        System.out.println(countEven("543276"));
     }
 
     public static String reverseWord(String word)
@@ -17,5 +18,19 @@ public class RecursionPractice
             String wordMinusLast = word.substring(0, lastIndex);
             return word.charAt(lastIndex) + reverseWord(wordMinusLast);
         }
+    }
+
+    public static int countEven(String num)
+    {
+        if (num.isEmpty())
+        {
+            return 0;
+        }
+        int firstNum = Integer.parseInt(num.substring(0, 1));
+        if (firstNum % 2 == 0)
+        {
+            return 1 + countEven(num.substring(1));
+        }
+        return countEven(num.substring(1));
     }
 }
