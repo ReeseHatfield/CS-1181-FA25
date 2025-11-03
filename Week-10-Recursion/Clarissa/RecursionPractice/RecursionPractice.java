2338bd9db8a0ecf520add33c95fc613f30da3fbb
Clarissa Milligan <milligan.s.clarissa@gmail.com>
2025-11-03 11:22:02 -0500

    more recursion

--- Week-10-Recursion/Clarissa/RecursionPractice/RecursionPractice.java
+++ Week-10-Recursion/Clarissa/RecursionPractice/RecursionPractice.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+
 public class RecursionPractice
 {
     public static void main(String[] args)
     {
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(-8);
+        nums.add(10);
+        nums.add(3);
         System.out.println(reverseWord("hello"));
         System.out.println(countEven("543276"));
+        System.out.println(findMax(nums));
     }
 
     public static String reverseWord(String word)
@@ -32,5 +40,29 @@ public class RecursionPractice
             return 1 + countEven(num.substring(1));
         }
         return countEven(num.substring(1));
+    }
+
+    public static <E extends Comparable<E>> E findMax(ArrayList<E> input)
+    {
+        // method should find and return the "max" value from the input
+        // (recursively)
+
+        if (input.isEmpty())
+        {
+            return null;
+        }
+        else if (input.size() == 1)
+        {
+            return input.getFirst();
+        }
+
+        E firstValue = input.removeFirst();
+        E max = findMax(input);
+
+        if (firstValue.compareTo(max) > 0)
+        {
+            return firstValue;
+        }
+        return max;
     }
 }
