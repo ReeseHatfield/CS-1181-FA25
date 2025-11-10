8eb000cebf2b2739279829989a4adb6cafca5e3a
ReeseHatfield <reesehatfield02@gmail.com>
2025-11-10 18:07:59 -0500

    nov 10

--- Week-12-SearchingSorting/Reese/Searching/src/Driver.java
+++ Week-12-SearchingSorting/Reese/Searching/src/Driver.java
@@ -1,14 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Driver {
 
 
+
     public static void main(String[] args) {
         
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1, 4, 6, 8, 3, 10));
+
+        Collections.sort(list); // complexity O(n * logn)
+        
+        System.out.println(contains(list, 7, 0, list.size() - 1));
+    }
+
+
+    public static boolean contains(List<Integer> searchMe, int target, int left, int right){
+
+        if(left > right){
+            return false;
+        }
+
+        int mid = (right + left) / 2;
+
+        if(searchMe.get(mid) == target){
+            return true;
+        }
+
+        if(searchMe.get(mid) < target) {
+            return contains(searchMe, target, mid + 1, right);
+        }
+        else {
+            return contains(searchMe, target, left, mid - 1);
+        }
+
+
+
     }
 
     
-    
+        
     public static int indexOfValue(List<String> searchMe, String target, int curIndex){
 
         System.out.println(searchMe);
@@ -17,7 +50,7 @@ public class Driver {
             return -1;
         }
 
-        if(target.equals(searchMe.get(0)){
+        if(target.equals(searchMe.get(0))){
             return curIndex;
         }
 
